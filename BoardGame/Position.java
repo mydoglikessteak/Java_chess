269c7a38bfd4f80bd009e30a2956c7668df95097
Vini35 <viniciusalex0303@gmail.com>
2022-03-12 16:10:02 -0300

    creating repository

---
+++ BoardGame/Position.java
@@ -0,0 +1,33 @@
+package BoardGame;
+
+ public class Position{
+    
+    private int row;
+    private int column;
+
+    public  Position(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getColumn(){
+        return column;
+    }
+
+    private void setRow(int row){
+        this.row = row;
+    }
+
+    private void setColumn(int column){
+        this.column = column;
+    }
+
+    @Override
+    public String toString(){
+        return "Position :   " + row  +","+ column;
+    }
+}
---
+++ position/main.java
@@ -0,0 +1,12 @@
+package position;
+import BoardGame.Position;
+public class main {
+
+    public static void main(String[] args) {
+        Position position = new Position(13,30);
+        System.out.println(position.toString());
+    }
+
+    
+    
+}
